e02182cff1d6fb641f69e8d02d3eae6153b8f8be
Trần Hoàng Hiệp <congchuahiep@gmail.com>
2025-08-20 23:28:25 +0700

    change home-carousel to gallery

--- js/gallery.js
+++ js/gallery.js
@@ -1,35 +1,35 @@
 $(function () {
 	let currentSlide = 0; // Chỉ mục của slide hiện tại
-	const totalSlides = $(".carousel__slide").length;
+	const totalSlides = $(".gallery__slide").length;
 
 	// Nhấn vào dấu chấm chỉ mục
-	$(".carousel__dot").on("click", function () {
+	$(".gallery__dot").on("click", function () {
 		currentSlide = $(this).data("slide");
-		updateCarousel();
+		updateGallery();
 	});
 
 	// Nhấn vào mũi tên
-	$(".carousel__arrow-btn").on("click", function () {
+	$(".gallery__arrow-btn").on("click", function () {
 		currentSlide = (currentSlide + 1) % totalSlides;
-		updateCarousel();
+		updateGallery();
 	});
 
-	function updateCarousel() {
-		$(".carousel__slide").removeClass("carousel__slide--active");
-		$(".carousel__slide").eq(currentSlide).addClass("carousel__slide--active");
+	function updateGallery() {
+		$(".gallery__slide").removeClass("gallery__slide--active");
+		$(".gallery__slide").eq(currentSlide).addClass("gallery__slide--active");
 
-		$(".carousel__dot").removeClass("carousel__dot--active");
-		$(".carousel__dot").eq(currentSlide).addClass("carousel__dot--active");
+		$(".gallery__dot").removeClass("gallery__dot--active");
+		$(".gallery__dot").eq(currentSlide).addClass("gallery__dot--active");
 
 		if ($(window).width() <= 768) {
 			// Mobile: Khoảng cách di chuyển = 100% chiều rộng màn hình
-			$(".carousel__track").css(
+			$(".gallery__track").css(
 				"transform",
 				`translateX(${-currentSlide * 100}%)`
 			);
 		} else {
 			// Desktop: Khoảng cách di chuyển = vị trí hiện tại nhân với (độ rộng của một slide + gap)
-			$(".carousel__track").css(
+			$(".gallery__track").css(
 				"transform",
 				`translateX(${-currentSlide * (372 + 24)}px)`
 			);
